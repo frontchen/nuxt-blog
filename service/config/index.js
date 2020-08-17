61c341ec97ace97761e352765487eb61c7b74e9a
陈振 <599422271@qq.com>
2020-08-17 20:33:55 +0800

    修改server服务端路由

--- service/config/index.js
+++ service/config/index.js
@@ -8,26 +8,39 @@ let dbconfig = {
     password: "123456"
   },
   production: {
-    host: "http://api.blog.chenzhen",
+    host: "http://api.blog.chenzhen.work",
     dbPort: 27017,
     user: "root",
     password: "123456"
   },
   uat: {
-    host: "http://api.blog.chenzhen",
+    host: "http://api.blog.chenzhen.work",
     dbPort: 27017,
     user: "root",
     password: "123456"
   }
 };
+let config = {
+  //服务器配置
+  development: {
+    host: "localhost",
+    port: 5000
+  },
+  production: {
+    host: "http://blog.chenzhen.work",
+    port: 5000
+  },
+  uat: {
+    host: "http://blog.chenzhen.work",
+    port: 5000
+  }
+};
 dbconfig = dbconfig[process.env.NODE_ENV];
+config = config[process.env.NODE_ENV];
 // token相关
 const jwtconfig = {
   secretKey: "secretkey",
   expiresIn: 60 * 60 * 24 * 1 // 一天
 };
 
-module.exports = {
-  dbconfig,
-  jwtconfig
-};
+export { dbconfig, jwtconfig, config };
