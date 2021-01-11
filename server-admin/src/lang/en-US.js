e43b18d46707d60d8c350b1c5702cc4a41acbc41
chenzhen <599422271@qq.com>
2021-01-11 23:46:12 +0800

    语言国际化

---
+++ server-admin/src/lang/en-US.js
@@ -0,0 +1,33 @@
+export default {
+  common: {
+    help: 'help',
+    message: 'message',
+    advisory: 'advisory',
+    companyName: 'company name',
+    msg: 'prompt',
+    deniedTips:
+      'You do not have permission to use this module, please contact the administrator'
+  },
+  menu: {
+    workM: 'work'
+  },
+  state: {
+    pending: '待审批',
+    approved: '已批准',
+    completed: '已完成',
+    expired: '已失效'
+  },
+  action: {
+    add: 'add',
+    del: 'delete',
+    edit: 'edit',
+    search: 'search',
+    cancal: 'cancal',
+    ok: '确定',
+    save: 'save',
+    approval: '审批',
+    remove: 'remove',
+    quit: 'logout',
+    close: 'close'
+  }
+}
--- server-admin/src/main.js
+++ server-admin/src/main.js
@@ -1,6 +1,7 @@
 import { createApp } from "vue";
 import App from "./App.vue";
 import ElementPlus from "element-plus";
+import VueI18n from "./lang";
 import "element-plus/lib/theme-chalk/index.css";
 
 import router from "./router";
@@ -8,9 +9,12 @@ import store from "./store";
 
 const app = createApp(App);
 app.config.productionTip = false;
-app.use(router);
-app.use(store);
-app.use(ElementPlus);
+app
+  .use(router)
+  .use(store)
+  .use(VueI18n)
+  .use(ElementPlus)
+  .use(VueI18n);
 app.mount("#app");
 // new Vue({
 //   render: h => h(App),
